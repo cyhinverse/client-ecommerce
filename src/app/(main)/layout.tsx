00eabf9c824648267b5da6469af0e02a86eab7a9
cyhinserve <quocanhlove199@gmail.com>
2026-01-28 18:48:46 +0700

    Revert "remove: ChatWidgetWrapper (AI Chatbot) from layout to fix conflict with Shop Chat"
    This reverts commit 03d561c7ec53abfcccca3f0311dc5fbdc5589c73.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@
 import { useAppSelector } from "@/hooks/hooks";
 import HeaderLayout from "@/components/layout/header/layout";
 import FooterLayout from "@/components/layout/footer/page";
+import ChatWidgetWrapper from "@/components/chatbot/ChatWidgetWrapper";
 import ChatPanel from "@/components/chat/ChatPanel";
 import { cn } from "@/lib/utils";
 
@@ -23,6 +24,9 @@ export default function MainLayout({
         <main className="flex-1">{children}</main>
         <FooterLayout />
       </div>
+
+      {/* AI Chat Sidebar */}
+      <ChatWidgetWrapper />
 
       {/* User-Shop Chat Components */}
       <ChatPanel />
